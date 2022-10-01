3a47f6042749ffc46fe5cae19d4b8a9e14601e75
Allyson Roch4 <114816292+wallyzinhub@users.noreply.github.com>
2022-10-01 13:43:27 -0300

    01

---
+++ main.py
@@ -0,0 +1,8 @@
+nome = input('Digite seu Nome? ')
+cpf = input('Digite seu CPF: ')
+endereço = input('Digite seu Endereço:')
+idade = input('Digite sua Idade: ')
+altura = input('Digite sua altura: ')
+telefone = input('Dgite seu Telefone:')
+
+print(nome, 'seu cpf é', cpf, 'seu endereço é', endereço, 'sua idade é', idade,'você tem', altura, 'de altura', 'seu telefone é', telefone)
